6bc8f49280c5905e11972207e7865691045c325c
Vladimir Zhelvis <v.zhelvis@adguard.com>
2021-08-19 13:23:04 +0300

    auto enable allowlist when disable blocking on site

--- Extension/src/background/filter/allowlist.js
+++ Extension/src/background/filter/allowlist.js
@@ -220,6 +220,13 @@ export const allowlist = (() => {
      */
     const allowlistUrl = function (url) {
         const domain = utils.url.getDomainName(url);
+
+        const allowlistEnabled = settings.getAllowlistEnabledState();
+
+        if (!allowlistEnabled) {
+            settings.setAllowlistEnabledState(true);
+        }
+
         if (isDefaultAllowlistMode()) {
             addToAllowlist(domain);
         } else {
